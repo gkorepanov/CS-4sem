93e892f48bb792eabbb1c7b428cf274a71e33620
George Korepanov <gkorepanov.gk@gmail.com>
2017-05-02 17:15:46 +0300

    Added functionality to set cpu affinity.
    Not tested yet

--- simpson/integral.c
+++ simpson/integral.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -5,6 +7,9 @@
 #include <pthread.h>
 #include "../tools/alerts1.h"
 #include <unistd.h>
+#include <errno.h>
+
+#include <sched.h>
 
 
 pthread_t* threads;
@@ -14,9 +19,24 @@ unsigned N;
 // field 'a' is used as thread rvalue
 typedef struct SimpsonData {
 	long double a, b;
+	int core_id;
 } SimpsonData;
 SimpsonData* data;
 
+
+// core_id = 0, 1, ... n-1, where n is the system's number of cores
+int stick_this_thread_to_core(int core_id) {
+   int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
+   if (core_id < 0 || core_id >= num_cores)
+      return EINVAL;
+
+   cpu_set_t cpuset;
+   CPU_ZERO(&cpuset);
+   CPU_SET(core_id, &cpuset);
+
+   pthread_t current_thread = pthread_self();
+   return pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset);
+}
 
 void arg_process(int argc, char** argv) {
 	if (argc != 4)
@@ -40,6 +60,9 @@ void* simpson(void* args) {
 			     h = (((SimpsonData*)args)->b - a)/steps,
 			     h2 = h/2,
 			     acc = 0;
+	int core_id = ((SimpsonData*)args)->core_id;
+	if(stick_this_thread_to_core(core_id))
+		ERROR("Sticking thread to specific core failed.")
 
 	for (unsigned i = 0; i < steps; ++i) {
 		acc += FUNC(a) + 4 * FUNC(a + h2) + FUNC(a + h);
@@ -62,10 +85,11 @@ int main(int argc, char** argv) {
 	long cores_num = sysconf(_SC_NPROCESSORS_ONLN);
 	
 	// A time to take stones away...
-	for (unsigned i = 0; i < N; ++i) {
+	for (unsigned int i = 0; i < N; ++i) {
 		data[i] = (SimpsonData) {
 			a + (b - a)/N * i,
-			a + (b - a)/N * (i+1)
+			a + (b - a)/N * (i+1),
+			i % cores_num
 		};
 		if (pthread_create(&threads[i], NULL, &simpson, &data[i]))
 			ERROR("Thread creation failed");

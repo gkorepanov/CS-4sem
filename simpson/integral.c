0e2114800e8e62fddbb1add43cfbed654f8f8cd1
Ivan Uteshev <uteshev@phystech.edu>
2017-05-02 11:16:49 +0300

    Simpson: it calcs

--- simpson/integral.c
+++ simpson/integral.c
@@ -1,18 +1,85 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-/*
- * Simpson integration
- */
-double Simpson(double(*f)(double), double a, double b) {
-    return (b - a) / 6 * (f(a) + 4 * f((a + b) / 2) + f(b));
+#include <unistd.h>
+#include <pthread.h>
+#include "../tools/alerts1.h"
+
+
+pthread_t* threads;
+double a, b;
+unsigned N;
+
+// field 'a' is used as thread rvalue
+typedef struct SimpsonData {
+	double a, b;
+} SimpsonData;
+SimpsonData* data;
+
+
+void arg_process(int argc, char** argv) {
+	if (argc != 4)
+		ERROR("Usage: integral [lower bound] [upper bound] [NUMBER OF THREADS]");
+
+	if (!sscanf(argv[1], "%lf", &a) || !sscanf(argv[2], "%lf", &b))
+		ERROR("Invalid argument (type <double>)");
+	if (!sscanf(argv[3], "%u", &N))
+		ERROR("Invalid argument (type <unsigned>)");
+
+	if (!(N==1 || N==2 || N==4 || N==8 || N==16 || N==32 || N==64))
+		ERROR("Invalid argument (type <unsigned>)\n\
+              NUMBER OF THREADS: expected 1, 2, 4, 8, 16, 32 or 64");
 }
 
-/*
- * Let's integrate some function
- */
-double f(double x) {
-	if (x)
-		return sin(x)/x;
-	else
-		return 1;
+
+// Simpson integration
+#define FUNC(x) (((x) == 0) ? 1 : (sin(x)/(x)))
+#define DEFAULT_SPLIT 33554432  // =2**25
+
+void* simpson(void* args) {
+	unsigned steps = DEFAULT_SPLIT/N;
+
+	double  a = ((SimpsonData*)args)->a,
+			h = (((SimpsonData*)args)->b - a)/steps,
+			h2 = h/2,
+			acc = 0;
+
+	for (unsigned i = 0; i < steps; ++i) {
+		acc += FUNC(a) + 4 * FUNC(a + h2) + FUNC(a + h);
+		a += h;
+	}
+
+	double* rvalue = &((SimpsonData*)args)->a;
+	*rvalue = acc * h/6;
+    pthread_exit(rvalue);
+}
+
+
+int main(int argc, char** argv) {
+	arg_process(argc, argv);
+
+	if (!(threads = malloc(N * sizeof(pthread_t))) || !(data = malloc(N * sizeof(SimpsonData))))
+		ERROR("Memory allocation failed");
+
+	// A time to take stones away...
+	for (unsigned i = 0; i < N; ++i) {
+		data[i] = (SimpsonData) {
+			a + (b - a)/N * i,
+			a + (b - a)/N * (i+1)
+		};
+		if (pthread_create(&threads[i], NULL, &simpson, &data[i]))
+			ERROR("Thread creation failed");
+	}
+
+	// ...and a time to get stones together
+	double S = 0, *r;
+	for (unsigned i = 0; i < N; ++i) {
+		if (pthread_join(threads[i], (void**) &r))
+			ERROR("Thread joining failed");
+		S += *r;
+	}
+	printf("%lf\n", S);
+
+	free(threads);
+	return 0;
 }
---
+++ tools/colors.h
@@ -0,0 +1,59 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+// Reset
+#define NORM         "\033[0m"
+
+// Format
+#define BOLD         "\033[1m"
+#define DIM          "\033[2m"
+#define UL           "\033[4m"
+#define INV          "\033[7m"
+#define HID          "\033[8m"
+
+#define UN_BOLD      "\033[21m"
+#define UN_DIM       "\033[22m"
+#define UN_UL        "\033[24m"
+#define UN_INV       "\033[27m"
+#define UN_HID       "\033[28m"
+
+
+// Text color
+#define DCOLOR       "\033[39m"
+#define BLACK        "\033[30m"
+#define RED          "\033[31m"
+#define GREEN        "\033[32m"
+#define YELLOW       "\033[33m"
+#define BLUE         "\033[34m"
+#define MAGENTA      "\033[35m"
+#define CYAN         "\033[36m"
+#define LGRAY        "\033[37m"
+#define DGRAY        "\033[90m"
+#define LRED         "\033[91m"
+#define LGREEN       "\033[92m"
+#define LYELLOW      "\033[93m"
+#define LBLUE        "\033[94m"
+#define LMAGENTA     "\033[95m"
+#define LCYAN        "\033[96m"
+#define WHITE        "\033[97m"
+
+// Background color
+#define BG_DCOLOR    "\033[49m"
+#define BG_BLACK     "\033[40m"
+#define BG_RED       "\033[41m"
+#define BG_GREEN     "\033[42m"
+#define BG_YELLOW    "\033[43m"
+#define BG_BLUE      "\033[44m"
+#define BG_MAGENTA   "\033[45m"
+#define BG_CYAN      "\033[46m"
+#define BG_LGRAY     "\033[47m"
+#define BG_DGRAY     "\033[100m"
+#define BG_LRED      "\033[101m"
+#define BG_LGREEN    "\033[102m"
+#define BG_LYELLOW   "\033[103m"
+#define BG_LBLUE     "\033[104m"
+#define BG_LMAGENTA  "\033[105m"
+#define BG_LCYAN     "\033[106m"
+#define BG_WHITE     "\033[107m"
+
+#endif // COLORS_H

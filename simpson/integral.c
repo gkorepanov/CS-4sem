57c680aaee9a0492f08e05d2bd8f2e3fc8ab5167
George Korepanov <korepanov.gm@phystech.edu>
2017-05-02 16:43:15 +0300

    First step on the way to proper scalability

--- simpson/integral.c
+++ simpson/integral.c
@@ -4,15 +4,16 @@
 #include <unistd.h>
 #include <pthread.h>
 #include "../tools/alerts1.h"
+#include <unistd.h>
 
 
 pthread_t* threads;
-double a, b;
+long double a, b;
 unsigned N;
 
 // field 'a' is used as thread rvalue
 typedef struct SimpsonData {
-	double a, b;
+	long double a, b;
 } SimpsonData;
 SimpsonData* data;
 
@@ -21,14 +22,10 @@ void arg_process(int argc, char** argv) {
 	if (argc != 4)
 		ERROR("Usage: integral [lower bound] [upper bound] [NUMBER OF THREADS]");
 
-	if (!sscanf(argv[1], "%lf", &a) || !sscanf(argv[2], "%lf", &b))
-		ERROR("Invalid argument (type <double>)");
+	if (!sscanf(argv[1], "%Lf", &a) || !sscanf(argv[2], "%Lf", &b))
+		ERROR("Invalid argument (type <long double>)");
 	if (!sscanf(argv[3], "%u", &N))
 		ERROR("Invalid argument (type <unsigned>)");
-
-	if (!(N==1 || N==2 || N==4 || N==8 || N==16 || N==32 || N==64))
-		ERROR("Invalid argument (type <unsigned>)\n\
-              NUMBER OF THREADS: expected 1, 2, 4, 8, 16, 32 or 64");
 }
 
 
@@ -39,17 +36,17 @@ void arg_process(int argc, char** argv) {
 void* simpson(void* args) {
 	unsigned steps = DEFAULT_SPLIT/N;
 
-	double  a = ((SimpsonData*)args)->a,
-			h = (((SimpsonData*)args)->b - a)/steps,
-			h2 = h/2,
-			acc = 0;
+	long double  a = ((SimpsonData*)args)->a,
+			     h = (((SimpsonData*)args)->b - a)/steps,
+			     h2 = h/2,
+			     acc = 0;
 
 	for (unsigned i = 0; i < steps; ++i) {
 		acc += FUNC(a) + 4 * FUNC(a + h2) + FUNC(a + h);
 		a += h;
 	}
 
-	double* rvalue = &((SimpsonData*)args)->a;
+	long double* rvalue = &((SimpsonData*)args)->a;
 	*rvalue = acc * h/6;
     pthread_exit(rvalue);
 }
@@ -61,6 +58,9 @@ int main(int argc, char** argv) {
 	if (!(threads = malloc(N * sizeof(pthread_t))) || !(data = malloc(N * sizeof(SimpsonData))))
 		ERROR("Memory allocation failed");
 
+	// get number of online cores
+	long cores_num = sysconf(_SC_NPROCESSORS_ONLN);
+	
 	// A time to take stones away...
 	for (unsigned i = 0; i < N; ++i) {
 		data[i] = (SimpsonData) {
@@ -72,13 +72,13 @@ int main(int argc, char** argv) {
 	}
 
 	// ...and a time to get stones together
-	double S = 0, *r;
+	long double S = 0, *r;
 	for (unsigned i = 0; i < N; ++i) {
 		if (pthread_join(threads[i], (void**) &r))
 			ERROR("Thread joining failed");
 		S += *r;
 	}
-	printf("%lf\n", S);
+	printf("%.18Lf\n", S);
 
 	free(threads);
 	return 0;

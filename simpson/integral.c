209db7e315c2419aafc253d144328cc2c3e2098b
George Korepanov <gkorepanov.gk@gmail.com>
2017-05-03 19:21:46 +0300

    Deleted redundant function

--- simpson/integral.c
+++ simpson/integral.c
@@ -31,20 +31,6 @@ typedef struct SimpsonData {
 SimpsonData* data;
 
 
-// core_id = 0, 1, ... n-1, where n is the system's number of cores
-int stick_this_thread_to_core(int core_id) {
-   int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
-   if (core_id < 0 || core_id >= num_cores)
-      return EINVAL;
-
-   cpu_set_t cpuset;
-   CPU_ZERO(&cpuset);
-   CPU_SET(core_id, &cpuset);
-
-   pthread_t current_thread = pthread_self();
-   return pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset);
-}
-
 void arg_process(int argc, char** argv) {
 	if (argc != 4)
 		ERROR("Usage: integral [lower bound] [upper bound] [NUMBER OF THREADS]");

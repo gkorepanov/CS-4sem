4442a13f36e49c78b0c39575944b66bcd9575c1d
George Korepanov <korepanov.gm@phystech.edu>
2017-05-05 13:49:49 +0300

    Removed tabs

--- simpson/integral.c
+++ simpson/integral.c
@@ -22,8 +22,8 @@
 
 
 typedef struct SimpsonData {
-	long double a;                         // is used as thread rvalue
-	cpu_set_t cpuset;
+    long double a;                         // is used as thread rvalue
+    cpu_set_t cpuset;
 } SimpsonData;
 
 SimpsonData* data;
@@ -45,166 +45,166 @@ void* simpson(void* args);
 
 int main(int argc, char** argv) {
 
-	arg_process(argc, argv);               // acquire left and right bounds (a, b) and number of lines N
-	cpu_process();                         // acquire lists of online cores and thread siblings
+    arg_process(argc, argv);               // acquire left and right bounds (a, b) and number of lines N
+    cpu_process();                         // acquire lists of online cores and thread siblings
 
-	if (!(threads = malloc(online_cpus_num * sizeof(pthread_t))) ||
-		!(data = malloc(online_cpus_num * sizeof(SimpsonData))))
-		ERROR("Memory allocation failed");
+    if (!(threads = malloc(online_cpus_num * sizeof(pthread_t))) ||
+        !(data = malloc(online_cpus_num * sizeof(SimpsonData))))
+        ERROR("Memory allocation failed");
 
-	// prepare variables for calculation
-	steps = DEFAULT_SPLIT / N;
-	h = (b - a) / DEFAULT_SPLIT;
-	h2 = h/2;
-	long double interval_start,
+    // prepare variables for calculation
+    steps = DEFAULT_SPLIT / N;
+    h = (b - a) / DEFAULT_SPLIT;
+    h2 = h/2;
+    long double interval_start,
                 interval_len = (b - a)/N;
-	unsigned j = 0, i;
-	
-	// A time to take stones away...
-	for (i = 0, interval_start = a;
-		 i < online_cpus_num;
-		 ++i, interval_start += interval_len, j++) {
+    unsigned j = 0, i;
+    
+    // A time to take stones away...
+    for (i = 0, interval_start = a;
+         i < online_cpus_num;
+         ++i, interval_start += interval_len, j++) {
 
-		// running the calculation on every physical core, but
-		// some threads are "fake" and are not actually used so that
-		// all the cores are loaded and Intel Turbo Boost don't distort the results
-		
-		while(!is_cpu_online[j]) ++j; // find next online physical core
+        // running the calculation on every physical core, but
+        // some threads are "fake" and are not actually used so that
+        // all the cores are loaded and Intel Turbo Boost don't distort the results
+        
+        while(!is_cpu_online[j]) ++j; // find next online physical core
 
-		data[i] = (SimpsonData) {
-			interval_start,
-			cpu_sets[j]
-		};
+        data[i] = (SimpsonData) {
+            interval_start,
+            cpu_sets[j]
+        };
 
-		PRINT("Running thread on core %u", j);
-	}
+        PRINT("Running thread on core %u", j);
+    }
 
-	for (i = 0; i < online_cpus_num; i++)
-		if (pthread_create(&threads[i], NULL, &simpson, &data[i]))
-			ERROR("Thread creation failed");
+    for (i = 0; i < online_cpus_num; i++)
+        if (pthread_create(&threads[i], NULL, &simpson, &data[i]))
+            ERROR("Thread creation failed");
 
-	// ...and a time to get stones together
-	long double S = 0, *r;
-	for (i = 0; i < online_cpus_num; ++i) {
-		if (pthread_join(threads[i], (void**) &r))
-			ERROR("Thread joining failed");
-		if (i < N)
-			S += *r;
-	}
-	printf("%.12Lf\n", S);
+    // ...and a time to get stones together
+    long double S = 0, *r;
+    for (i = 0; i < online_cpus_num; ++i) {
+        if (pthread_join(threads[i], (void**) &r))
+            ERROR("Thread joining failed");
+        if (i < N)
+            S += *r;
+    }
+    printf("%.12Lf\n", S);
 
-	free(threads);
-	free(data);
-	return 0;
+    free(threads);
+    free(data);
+    return 0;
 }
 
 
 
 void arg_process(int argc, char** argv) {
-	if (argc != 4)
-		ERROR("Usage: integral [lower bound] [upper bound] [NUMBER OF THREADS]");
+    if (argc != 4)
+        ERROR("Usage: integral [lower bound] [upper bound] [NUMBER OF THREADS]");
 
-	if (!sscanf(argv[1], "%Lf", &a) || !sscanf(argv[2], "%Lf", &b))
-		ERROR("Invalid argument (type <long double>)");
-	if (!sscanf(argv[3], "%u", &N))
-		ERROR("Invalid argument (type <unsigned>)");
+    if (!sscanf(argv[1], "%Lf", &a) || !sscanf(argv[2], "%Lf", &b))
+        ERROR("Invalid argument (type <long double>)");
+    if (!sscanf(argv[3], "%u", &N))
+        ERROR("Invalid argument (type <unsigned>)");
 }
 
 
 void cpu_process() {
-	memset(&is_virtual_cpu_online, 0, sizeof(is_virtual_cpu_online));
+    memset(&is_virtual_cpu_online, 0, sizeof(is_virtual_cpu_online));
 
-	int fd;
-	ERRTEST(fd = open("/sys/devices/system/cpu/online", O_RDONLY));
-	char online_str[MAX_SYS_FILE_LENGTH];
-	ERRTEST(read(fd, online_str, MAX_SYS_FILE_LENGTH));
-	close(fd);
+    int fd;
+    ERRTEST(fd = open("/sys/devices/system/cpu/online", O_RDONLY));
+    char online_str[MAX_SYS_FILE_LENGTH];
+    ERRTEST(read(fd, online_str, MAX_SYS_FILE_LENGTH));
+    close(fd);
 
-	unsigned left_bound, right_bound;
-	char *saveptr1 = NULL,
+    unsigned left_bound, right_bound;
+    char *saveptr1 = NULL,
          *saveptr2 = NULL,
          *str = strtok_r(online_str, ",", &saveptr1);
 
-	do {
-		str = strtok_r(str, "-", &saveptr2);
-		if (!sscanf(str, "%u", &left_bound))
-			ERROR("Failed to parse online cores file");
-		right_bound = left_bound;
+    do {
+        str = strtok_r(str, "-", &saveptr2);
+        if (!sscanf(str, "%u", &left_bound))
+            ERROR("Failed to parse online cores file");
+        right_bound = left_bound;
 
-		str = strtok_r(NULL, "-", &saveptr2);
-		if (str)
-			if (!sscanf(str, "%u", &right_bound))
-				ERROR("Failed to parse online cores file");
+        str = strtok_r(NULL, "-", &saveptr2);
+        if (str)
+            if (!sscanf(str, "%u", &right_bound))
+                ERROR("Failed to parse online cores file");
 
-		// update table
-		for (unsigned i = left_bound; i <= right_bound; i++)
-			is_virtual_cpu_online[i] = 1;
-	} while ((str = strtok_r(NULL, ",", &saveptr1)));
+        // update table
+        for (unsigned i = left_bound; i <= right_bound; i++)
+            is_virtual_cpu_online[i] = 1;
+    } while ((str = strtok_r(NULL, ",", &saveptr1)));
 
 
-	memset(&is_cpu_online, 0, sizeof(is_cpu_online));
-	for (unsigned i = 0; i < MAX_CPUS; i++)
-		CPU_ZERO(&cpu_sets[i]);
-	
-	char filename[256],
+    memset(&is_cpu_online, 0, sizeof(is_cpu_online));
+    for (unsigned i = 0; i < MAX_CPUS; i++)
+        CPU_ZERO(&cpu_sets[i]);
+    
+    char filename[256],
          core_str[10];
-	int  core_id;
+    int  core_id;
 
-	for (unsigned i = 0; i < MAX_CPUS; i++) {
-		if (!is_virtual_cpu_online[i])
-			continue;
+    for (unsigned i = 0; i < MAX_CPUS; i++) {
+        if (!is_virtual_cpu_online[i])
+            continue;
 
-		// read the corresponding physical core number
-		sprintf(filename, "/sys/devices/system/cpu/cpu%d/topology/core_id", i);
-		ERRTEST(fd = open(filename, O_RDONLY));
-		ERRTEST(read(fd, core_str, 10));
-		close(fd);
+        // read the corresponding physical core number
+        sprintf(filename, "/sys/devices/system/cpu/cpu%d/topology/core_id", i);
+        ERRTEST(fd = open(filename, O_RDONLY));
+        ERRTEST(read(fd, core_str, 10));
+        close(fd);
 
-		if (!sscanf(core_str, "%d", &core_id))
-			ERROR("Failed to parse topology/core_id file");
+        if (!sscanf(core_str, "%d", &core_id))
+            ERROR("Failed to parse topology/core_id file");
 
-		// update table with online physical cores and
-		// increase number of online physical cores by 1
-		if (!is_cpu_online[core_id]) {
-			is_cpu_online[core_id] = 1;
-			online_cpus_num++;
+        // update table with online physical cores and
+        // increase number of online physical cores by 1
+        if (!is_cpu_online[core_id]) {
+            is_cpu_online[core_id] = 1;
+            online_cpus_num++;
 
-			// bind virtual cpu to physical one
-			CPU_SET(i, &cpu_sets[core_id]);
-			PRINT("Binding %d virtual cpu to %d physical core", i, core_id);
-		}
-	}
+            // bind virtual cpu to physical one
+            CPU_SET(i, &cpu_sets[core_id]);
+            PRINT("Binding %d virtual cpu to %d physical core", i, core_id);
+        }
+    }
 
-	if (online_cpus_num < N)
-		ERROR("The number of physical cores online is less than requested number of threads");
+    if (online_cpus_num < N)
+        ERROR("The number of physical cores online is less than requested number of threads");
 }
 
 
 void* simpson(void* args) {
-	cpu_set_t cpuset = ((SimpsonData*)args)->cpuset;
-	pthread_t current_thread = pthread_self();
-   	if (pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset))
-		ERROR("Sticking thread to specific core failed");
+    cpu_set_t cpuset = ((SimpsonData*)args)->cpuset;
+    pthread_t current_thread = pthread_self();
+       if (pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset))
+        ERROR("Sticking thread to specific core failed");
 
-	long double lh = h,
+    long double lh = h,
                 lh2 = h2,
                 a = ((SimpsonData*)args)->a,
-				b = a + lh2,
-				c = a + lh,
-				func_a = FUNC(a),
-				func_c,
-				sum = 0;
+                b = a + lh2,
+                c = a + lh,
+                func_a = FUNC(a),
+                func_c,
+                sum = 0;
 
-	for (unsigned i = 0; i < steps; ++i) {
-		func_c = FUNC(c);
-		sum += func_a + 4 * FUNC(b) + func_c;
-		func_a = func_c;
-		a = c;
-		b += lh;
-		c += lh;
-	}
+    for (unsigned i = 0; i < steps; ++i) {
+        func_c = FUNC(c);
+        sum += func_a + 4 * FUNC(b) + func_c;
+        func_a = func_c;
+        a = c;
+        b += lh;
+        c += lh;
+    }
 
-	long double* rvalue = &((SimpsonData*)args)->a;
-	*rvalue = sum * h/6;
+    long double* rvalue = &((SimpsonData*)args)->a;
+    *rvalue = sum * h/6;
     pthread_exit(rvalue);
 }
